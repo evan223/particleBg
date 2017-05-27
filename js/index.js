d185e0294e4c0aa6df61fc6516ab99af5060d07c
RAPOO\Xiaoyuan Ling <lingxiaoyuan@rapoo.com>
2017-05-27 18:04:42 +0800

    fisrt commit

--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 ctx.fillStyle = "blue";
 ctx.fillRect(0,0,80,100);
+
+
+
+class Circle {
+	constructor (x, y) { // 创建圆对象
+		this.x = x;
+		this.y = y;
+		this.r = Math.random()*10;
+		this._mx = Math.random();
+		this._my = Math.random();
+	},
+
+}
